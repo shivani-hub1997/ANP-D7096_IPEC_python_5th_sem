424bd79211a6145e2b07eda681db08478962a237
shivani-hub1997 <shivani9891904028@gmail.com>
2026-06-28 13:33:39 +0530

    Program to anylize the result of online examination

---
+++ Lab_work/Selection_satement_26th_June/Online_examination_result_anylizer.py
@@ -0,0 +1,26 @@
+"""--------------------------online Examination Result Analyzer---------------------------------- 
+Problem Statement A student appears in 5 subjects.
+ Rules: 
+ • Minimum 40 marks in every subject to pass.  
+ • Distinction → Average ≥ 75  
+ • First Division→ Average ≥ 60  
+ • Second Division → Average ≥ 50  
+ • Pass  → Average ≥ 40  
+ • Fail if any subject score < 40  
+ 
+ Sample Input 
+ Hindi : 85 
+ English : 78 
+ Mathematics : 82 
+ Science : 75 
+ Computer : 90 
+ 
+ Sample Output 
+ Average Marks: 82.0 
+ Result: PASS Classification: Distinction """
+
+
+#-------------------------------coding section--------------------------------
+
+#take inputs from the user
+
