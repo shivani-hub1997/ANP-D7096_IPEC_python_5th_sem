c22f8a142f297ee4d102e20d70b3fd653bd6f887
shivani-hub1997 <shivani9891904028@gmail.com>
2026-07-08 10:51:02 +0530

    Example 1

---
+++ Class_work/Day10_Modules_8th_july/example1.py
@@ -0,0 +1,26 @@
+#importing module
+import numericcalculation
+#-------------------------------------------
+num1 = 5
+num2 = 10
+print("Sum of",num1,"and",num2,"is:",numericcalculation.calculate_addition(num1,num2))
+#-------------------------------------------
+print("Difference of",num1,"and",num2,"is:",numericcalculation.calculate_difference(num1,num2))
+#-------------------------------------------
+print("Multiplication of",num1,"and",num2,"is:",numericcalculation.calculate_multiplication(num1,num2))
+#-------------------------------------------
+print("Division of",num1,"and",num2,"is:",numericcalculation.calculate_division(num1,num2))
+#-------------------------------------------
+print("Remainder of",num1,"and",num2,"is:",numericcalculation.calculate_remainder(num1,num2))
+#-------------------------------------------
+
+
+#-------------------------------------------
+"""
+Output:
+Sum of 5 and 10 is:15
+Difference of 5 and 10 is:-5
+Multipication of 5 and 10 is:50
+Division of 5 and 10 is:0.5
+Remainder of 5 and 10 is:5
+--------------------------------------------"""
