0f1d583394dae7a1c5b47d801a5d05a9057c951e
shivani-hub1997 <shivani9891904028@gmail.com>
2026-07-08 13:07:34 +0530

    Geometry Module

---
+++ Lab_work/lab_practice_problems_8th_july/twodfigures.py
@@ -0,0 +1,31 @@
+#function to calculate area of triangle
+def calculate_area(base,height):
+    return((1/2) *base *height)
+#------------------------------
+#function to calculate perimeter of triangle
+def triangle_perimeter(side1, side2, side3):
+    return(side1 + side2 + side3)
+#-------------------------------
+# function to calculate area of circle
+def circle_area(radius):
+    return((3.14) *radius *radius)
+#-------------------------------
+#function to calculate perimeter of circle
+def circle_perimeter(radius):
+    return((2*3.14) *radius)
+#-------------------------------
+# function to calculate area of square
+def square_area(side):
+    return(side * side)
+#-------------------------------
+#function to calculate perimeter of square
+def square_perimeter(side):
+    return(4*side)
+#-------------------------------
+#function to calculate area of recentangle
+def rectangle_area(length, breadth):
+    return(length*breadth)
+#-------------------------------
+#function to calculate perimeter of rectangle
+def rectangle_perimeter(length, breadth):
+    return(2*(length + breadth))
