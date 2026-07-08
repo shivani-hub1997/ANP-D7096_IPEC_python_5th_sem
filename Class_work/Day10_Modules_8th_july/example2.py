403d8cc6881eda79de4c31c5783423de135911bb
shivani-hub1997 <shivani9891904028@gmail.com>
2026-07-08 10:51:52 +0530

    Example 2

---
+++ Class_work/Day10_Modules_8th_july/example2.py
@@ -0,0 +1,25 @@
+#importing module
+from numericcalculation import*
+#-------------------------------------
+a = 5
+b = 20
+print("Sum of",a,"and",b,"is:",calculate_addition(a,b))
+#-------------------------------------
+print("Difference of",a,"and",b,"is:",calculate_difference(a,b))
+#-------------------------------------
+print("Multiplcation of",a,"and",b,"is:",calculate_multiplication(a,b))
+#-------------------------------------
+print("Division of",a,"and",b,"is:",calculate_division(a,b))
+#-------------------------------------
+print("Remainder of",a,"and",b,"is:",calculate_remainder(a,b))
+#-------------------------------------
+
+
+#-------------------------------------
+"""
+Output:
+Sum of 5 and 20 is:25
+Difference of 5 and 20 is:-15
+Multiplication of 5 and 20 is:100
+Division of 5 and 20 is:0.25
+Remainder of 5 and 20 is:5"""
