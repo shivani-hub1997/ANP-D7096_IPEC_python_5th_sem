51cc53fe0f0a4a3daf33d523df46d7b6bee3eb10
shivani-hub1997 <shivani9891904028@gmail.com>
2026-07-08 10:52:44 +0530

    Example 4

---
+++ Class_work/Day10_Modules_8th_july/example4.py
@@ -0,0 +1,18 @@
+#importing module
+import numbericcalculation as nc
+#--------------------------------
+a = 5
+b = 15
+print("Sum of",a,"and",b,"is:",nc.calculate_addition(a,b))
+#--------------------------------
+m = 50
+n = 40
+print("Difference of",a,"and",b,"is:",nc.calculate_difference(m,n))
+#--------------------------------
+
+#--------------------------------
+"""
+Output:
+Sum of 5 and 15 is:20
+Difference of 50 and 40 is:10
+---------------------------------"""
