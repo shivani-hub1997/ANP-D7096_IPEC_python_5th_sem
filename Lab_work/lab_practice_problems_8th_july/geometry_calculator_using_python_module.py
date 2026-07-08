8eca80c8b11d17028ce9a48fac1456172f8c651d
shivani-hub1997 <shivani9891904028@gmail.com>
2026-07-08 13:04:10 +0530

    Geometry calculator

---
+++ Lab_work/lab_practice_problems_8th_july/geometry_calculator_using_python_module.py
@@ -0,0 +1,50 @@
+"""Problem Statement: 
+Geometry Calculator Using Python Module 
+Develop a menu-driven Python application that demonstrates the use of User-Defined 
+Modules and Functions. 
+Requirements 
+You are required to create a Python module named twodfigures.py that contains functions 
+to perform the following calculations for different two-dimensional shapes: 
+------------------------------------------------
+• Triangle 
+o Calculate Area 
+o Calculate Perimeter 
+------------------------------------------------
+• Circle 
+o Calculate Area 
+o Calculate Circumference (Perimeter) 
+-----------------------------------------------
+• Square 
+o Calculate Area 
+o Calculate Perimeter 
+-----------------------------------------------
+• Rectangle 
+o Calculate Area 
+o Calculate Perimeter 
+-----------------------------------------------
+Create another Python file named operations.py that imports the twodfigures module 
+and performs the following tasks: 
+------------------------------------------------
+1. Display a menu for selecting one of the following figures: 
+o Square 
+o Circle 
+o Triangle 
+o Rectangle 
+------------------------------------------------
+2. Based on the user's choice, ask for the required dimensions of the selected figure. 
+o Example: 
+▪ Circle → Radius 
+▪ Square → Side 
+▪ Rectangle → Length and Breadth 
+▪ Triangle → Three sides (for perimeter) and Base & Height (for area)
+------------------------------------------------- 
+3. Display a second menu to choose the required operation: 
+o Area 
+o Perimeter 
+-------------------------------------------------
+4. Call the appropriate function from the twodfigures module based on the user's selections. 
+-------------------------------------------------
+5. Display the calculated result in a user-friendly format. 
+--------------------------------------------------
+6. Allow the user to perform multiple calculations until they choose to exit the application. 
+----------------------------------------------------------------------------------"""
