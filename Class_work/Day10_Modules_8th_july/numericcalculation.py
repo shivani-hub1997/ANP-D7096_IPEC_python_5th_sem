0acfc1423814330482cfc1e22f6da70a84ebc766
shivani-hub1997 <shivani9891904028@gmail.com>
2026-07-08 10:49:57 +0530

    Module

---
+++ Class_work/Day10_Modules_8th_july/numericcalculation.py
@@ -0,0 +1,20 @@
+#function to calculate addition of two number
+def calculate_addition(num1,num2):
+    return (num1+num2)
+#-------------------------------------------
+#function to calculate difference of two number
+def calculate_difference(num1,num2):
+    return (num1-num2)
+#-------------------------------------------
+#function to calculate multiplication of two number
+def calculate_multiplication(num1,num2):
+    return (num1*num2)
+#-------------------------------------------
+#function to calculate division of two number
+def calculate_division(num1,num2):
+    return (num1/num2)
+#-------------------------------------------
+#function to calculate remainder of two number
+def calculate_remainder(num1,num2):
+    return (num1%num2)
+#-------------------------------------------
